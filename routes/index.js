3959548666a2bf806096b5b8a8e00cf917b22c0c
nevesvasco <a043335@umaia.pt>
2024-05-08 21:05:15 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,8 @@ app.use(passport.session());
 
 // Configure Google OAuth 2.0 strategy
 passport.use(new GoogleStrategy({
-    clientID: '146954126349-jlp6bek411g29mj7dl27p70mssiihf9v.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: 'uisdagh',
+    clientSecret: 'ouweht',
     callbackURL: 'http://localhost:8080/auth/google/callback',
 },
 (accessToken, refreshToken, profile, done) => {
