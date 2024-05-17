e0fbc641361fbc8f1a7d6256f7559959dd4d483c
DanielNunesPT <131387962+DanielNunesPT@users.noreply.github.com>
2024-05-17 21:00:36 +0100

    update ao plano service e contoller para resolver issue com plano post

--- controllers/planoController.js
+++ controllers/planoController.js
@@ -3,52 +3,47 @@
 var utils = require('../utils/writer.js');
 var PlanoS = require('../service/PlanoService.js');
 
-module.exports.planoGET = async function planoGET (req, res, next) {
-  PlanoS.planoGET()
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.planoGET = async function planoGET(req, res, next) {
+  try {
+    const response = await PlanoS.planoGET();
+    utils.writeJson(res, response, 200);
+  } catch (error) {
+    utils.writeJson(res, { error: error.message }, 500);
+  }
 };
 
-module.exports.planoPOST = async function planoPOST (req, res, next) {
-  await PlanoS.planoPOST(req.body)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.planoPOST = async function planoPOST(req, res, next) {
+  try {
+    const response = await PlanoS.planoPOST(req.body);
+    utils.writeJson(res, response, 201);
+  } catch (error) {
+    utils.writeJson(res, { error: error.message }, 500);
+  }
 };
 
-module.exports.plano_idDELETE = async function plano_idDELETE (req, res, next) {
-  await PlanoS.plano_idDELETE(req.params.id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.plano_idDELETE = async function plano_idDELETE(req, res, next) {
+  try {
+    const response = await PlanoS.plano_idDELETE(req.params.id);
+    utils.writeJson(res, response, 200);
+  } catch (error) {
+    utils.writeJson(res, { error: error.message }, 500);
+  }
 };
 
-module.exports.plano_idGET = async function plano_idGET (req, res, next) {
-  await PlanoS.plano_idGET(req.params.id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.plano_idGET = async function plano_idGET(req, res, next) {
+  try {
+    const response = await PlanoS.plano_idGET(req.params.id);
+    utils.writeJson(res, response, 200);
+  } catch (error) {
+    utils.writeJson(res, { error: error.message }, 500);
+  }
 };
 
-module.exports.plano_idPUT = async function plano_idPUT (req, res, next) {
-  await PlanoS.plano_idPUT(req.body,req.params.id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.plano_idPUT = async function plano_idPUT(req, res, next) {
+  try {
+    const response = await PlanoS.plano_idPUT(req.body, req.params.id);
+    utils.writeJson(res, response, 200);
+  } catch (error) {
+    utils.writeJson(res, { error: error.message }, 500);
+  }
 };
--- service/PlanoService.js
+++ service/PlanoService.js
@@ -1,19 +1,13 @@
 'use strict';
 var mysql = require("../utils/db.js")
 
-/**
- *
- * id Long 
- * returns List
- **/
 exports.cliente_idplanoGET = function(id) {
-  return new Promise(function(resolve, reject) {
-    mysql.query("SELECT * FROM cliente c JOIN plano p ON c.plano = p.id WHERE c.id = ?",[id], function (err, res) {
+  return new Promise((resolve, reject) => {
+    mysql.query("SELECT * FROM cliente c JOIN plano p ON c.plano = p.id WHERE c.id = ?", [id], function (err, res) {
       if (err) {
         console.log(err);
-        reject (err);
-      }
-      else {
+        reject(err);
+      } else {
         console.log(res);
         resolve(res);
       }
@@ -21,18 +15,13 @@ exports.cliente_idplanoGET = function(id) {
   });
 }
 
-/**
- *
- * returns List
- **/
 exports.planoGET = function() {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     mysql.query("SELECT * FROM plano", function (err, res) {
       if (err) {
         console.log(err);
-        reject (err);
-      }
-      else {
+        reject(err);
+      } else {
         console.log(res);
         resolve(res);
       }
@@ -40,63 +29,44 @@ exports.planoGET = function() {
   });
 }
 
-
-/**
- *
- * body Plano Plano que precisa ser adicionado , editado ou removido do negocio (optional)
- * no response value expected for this operation
- **/
 exports.planoPOST = function(body) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     console.log(body);
-    mysql.query("INSERT INTO plano (tipo_de_plano, periodicidade, preco, armazenamento, numero_de_contas_email, numero_de_dominios, largura_de_banda, fidelizacao ) Values(?,?,?,?,?,?,?,?)", [body.tipo_de_plano, body.periodicidade, body.preco, body.armazenamento, body.numero_de_contas_email, body.numero_de_dominios, body.largura_de_banda, body.fidelizacao], function (err, res) {
+    mysql.query("INSERT INTO plano (tipo_de_plano, periodicidade, preco, armazenamento, numero_de_contas_email, numero_de_dominios, largura_de_banda, fidelizacao) VALUES(?,?,?,?,?,?,?,?)", 
+    [body.tipo_de_plano, body.periodicidade, body.preco, body.armazenamento, body.numero_de_contas_email, body.numero_de_dominios, body.largura_de_banda, body.fidelizacao], 
+    function (err, res) {
       if (err) {
         console.log(err);
-        reject (err);
+        reject(err);
       } else {
         console.log(res.insertId);
-        resolve (res.insertId);
+        resolve({ id: res.insertId });
       }
     });
   });
 }
 
-
-/**
- *
- * id Long 
- * no response value expected for this operation
- **/
 exports.plano_idDELETE = function(id) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     mysql.query("DELETE FROM plano WHERE id = ?", [id], function (err, res) {
       if (err || !res.affectedRows) {
         console.log(err); 
-        console.log (res);
-        reject();
-      }
-      else {
-        console.log (res);
-        resolve ({"deleted": id});
+        reject(new Error('Failed to delete'));
+      } else {
+        console.log(res);
+        resolve({ deleted: id });
       }
     });
   });
 }
 
-
-/**
- *
- * id Long 
- * returns plano
- **/
 exports.plano_idGET = function(id) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     mysql.query("SELECT * FROM plano WHERE id = ?", [id], function (err, res) {
       if (err) {
         console.log(err);
-        reject (err);
-      }
-      else {
+        reject(err);
+      } else {
         console.log(res);
         resolve(res[0]);
       }
@@ -104,26 +74,19 @@ exports.plano_idGET = function(id) {
   });
 }
 
-
-/**
- *
- * body Plano Plano que precisa ser adicionado , editado ou removido do negocio (optional)
- * id Long 
- * no response value expected for this operation
- **/
-exports.plano_idPUT = function(body,id) {
-  return new Promise(function(resolve, reject) {
+exports.plano_idPUT = function(body, id) {
+  return new Promise((resolve, reject) => {
     console.log(body);
-    mysql.query("UPDATE plano set tipo_de_plano = COALESCE( ?, tipo_de_plano), periodicidade = COALESCE( ?, periodicidade), preco = COALESCE( ?, preco), armazenamento = COALESCE( ?, armazenamento), numero_de_contas_email = COALESCE( ?, numero_de_contas_email), numero_de_dominios = COALESCE( ?, numero_de_dominios), largura_de_banda = COALESCE( ?, largura_de_banda), fidelizacao = COALESCE( ?, fidelizacao) where id= ?", [body.tipo_de_plano, body.periodicidade, body.preco, body.armazenamento, body.numero_de_contas_email, body.numero_de_dominios, body.largura_de_banda, body.fidelizacao, id], function (err, res) {
+    mysql.query("UPDATE plano SET tipo_de_plano = COALESCE(?, tipo_de_plano), periodicidade = COALESCE(?, periodicidade), preco = COALESCE(?, preco), armazenamento = COALESCE(?, armazenamento), numero_de_contas_email = COALESCE(?, numero_de_contas_email), numero_de_dominios = COALESCE(?, numero_de_dominios), largura_de_banda = COALESCE(?, largura_de_banda), fidelizacao = COALESCE(?, fidelizacao) WHERE id = ?", 
+    [body.tipo_de_plano, body.periodicidade, body.preco, body.armazenamento, body.numero_de_contas_email, body.numero_de_dominios, body.largura_de_banda, body.fidelizacao, id], 
+    function (err, res) {
       if (err) {
         console.log(err);
-        reject (err);
-      }
-      else {
+        reject(err);
+      } else {
         console.log(res);
-        resolve(id);
+        resolve({ updated: id });
       }
     });
   });
 }
-
